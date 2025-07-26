3ee1d491cb69bea5a3225fe790cc8bc6e8be0392
Shraddha Singh <shraddhasingh.cse2024@gmail.com>
2025-07-26 11:07:07 +0530

    Update Selection Sort - Striver DSA

--- Sorting/01_selection_sort.cpp
+++ Sorting/01_selection_sort.cpp
@@ -27,7 +27,7 @@ int main() {
   for(int i=0;i<n;i++){
   cin>>arr[i];
   }
-    selectionSort(arr, n);
+    selection_sort(arr, n);
 
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
