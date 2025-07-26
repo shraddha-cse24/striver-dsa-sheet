419e4e29e32b596d4f703cd14d4f04f35c687729
Shraddha Singh <shraddhasingh.cse2024@gmail.com>
2025-07-26 11:06:12 +0530

    Added Bubble Sort - Striver DSA

---
+++ Sorting/02_bubble_sort.cpp
@@ -0,0 +1,38 @@
+// 02_bubble_sort.cpp
+// Bubble Sort implementation - Striver DSA Sheet
+// Author: Shraddha Singh
+#include <bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        int didswap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didswap = 1;
+            }
+        }
+        if (didswap == 0)
+        {
+            break;
+        }
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+  cin>>arr[i];
+  }
+    bubble_sort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
